8204997b906ed60ed9045b22d8ff550a49404be1
Albert Babaiani <babaianialbert@gmail.com>
2026-06-04 12:31:41 +0400

    add mobile category section in home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  categories = signal([
+    {
+      title: 'HEADPHONES',
+      thumbnailUrl: 'images/shared/desktop/image-category-thumbnail-headphones.png',
+      link: 'headphones',
+    },
+    {
+      title: 'SPEAKERS',
+      thumbnailUrl: 'images/shared/desktop/image-category-thumbnail-speakers.png',
+      link: 'speakers',
+    },
+    {
+      title: 'EARPHONES',
+      thumbnailUrl: 'images/shared/desktop/image-category-thumbnail-earphones.png',
+      link: 'earphones',
+    },
+  ]);
+}
