4b62c5811f147ab7e326202cddbb4e2d4cafd523
Albert Babaiani <babaianialbert@gmail.com>
2026-06-04 16:10:35 +0400

    add responsiveness on featured products

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -26,4 +26,36 @@ export class Home {
       link: 'earphones',
     },
   ]);
+
+  featuredProducts = signal({
+    zx9: {
+      title: `ZX9 SPEAKER`,
+      description:
+        'Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.',
+      thumbnailUrls: {
+        mobile: 'images/home/mobile/image-speaker-zx9.png',
+        tablet: 'images/home/tablet/image-speaker-zx9.png',
+        desktop: 'images/home/desktop/image-speaker-zx9.png',
+      },
+      link: '/speakers/zx9',
+    },
+    zx7: {
+      title: `ZX7 SPEAKER`,
+      thumbnailUrls: {
+        mobile: 'images/home/mobile/image-speaker-zx7.jpg',
+        tablet: 'images/home/tablet/image-speaker-zx7.jpg',
+        desktop: 'images/home/desktop/image-speaker-zx7.jpg',
+      },
+      link: '/speakers/zx7',
+    },
+    yx1: {
+      title: `YX1 EARPHONES`,
+      thumbnailUrls: {
+        mobile: 'images/home/mobile/image-earphones-yx1.jpg',
+        tablet: 'images/home/tablet/image-earphones-yx1.jpg',
+        desktop: 'images/home/desktop/image-earphones-yx1.jpg',
+      },
+      link: '/earphones/yx1',
+    },
+  });
 }
