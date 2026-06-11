eb465ba24965a22579edaf1a35d254a1f6967d7c
Albert Babaiani <babaianialbert@gmail.com>
2026-06-11 13:12:47 +0400

    add fully functional categories and abouts us components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutUs } from './shared/ui/about-us/about-us';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutUs],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('audiophile');
