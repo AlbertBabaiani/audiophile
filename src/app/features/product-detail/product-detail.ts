94f1572085542e8ed4b20321ed5091628d014ae1
Albert Babaiani <babaianialbert@gmail.com>
2026-06-16 13:07:44 +0400

    add product previow on mobile

--- src/app/features/product-detail/product-detail.ts
+++ src/app/features/product-detail/product-detail.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, Location } from '@angular/common';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.scss',
 })
-export class ProductDetail {}
+export class ProductDetail {
+  private readonly location = inject(Location);
+
+  goBack(): void {
+    this.location.back();
+  }
+}
